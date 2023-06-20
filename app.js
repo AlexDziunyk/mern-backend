9aa00dc440c1a0707d0958dcdfe29d49255be84f
Oleksandr Dziunyk <alexdziunyk@gmail.com>
2023-06-20 18:06:19 +0300

    fix

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/api/user', userRoutes)
 //connect to mongodb
 mongoose.connect(process.env.MONGO_DB)
     .then(() => {
-        app.listen(process.env.PORT, () => {
+        app.listen(process.env.PORT || 4000, () => {
             console.log('starting the server')
         })
     })
